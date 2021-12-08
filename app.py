0b43bb62b1e5519808628a6dc010aaff360aafe6
Muhammad Taufik Hidayat <79823539+Taufik322@users.noreply.github.com>
2021-12-08 23:48:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def send_uploaded_image(filename=''):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(threaded=True, port=5000)
+    app.run(debug=True)
